b1ea1eae3e99462103ae61dd517e18dab3b14bed
yeni-choi <yewonchoi@ewhain.net>
2023-11-11 13:21:35 +0900

    feat: create login dto

---
+++ src/main/java/com/ceos18/springboot/login/request/LoginRequestDto.java
@@ -0,0 +1,16 @@
+package com.ceos18.springboot.login.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class LoginRequestDto {
+    private String email;
+
+    private String password;
+}
