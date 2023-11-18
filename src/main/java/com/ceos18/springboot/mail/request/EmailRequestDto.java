1016ff6d1bfb083dec00f72659986c1031f32c02
yeni-choi <yewonchoi@ewhain.net>
2023-11-18 21:43:29 +0900

    feat: create email Dtos

---
+++ src/main/java/com/ceos18/springboot/mail/request/EmailRequestDto.java
@@ -0,0 +1,18 @@
+package com.ceos18.springboot.mail.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class EmailRequestDto {
+
+    @NotBlank(message = "이메일은 필수 입력값입니다.")
+    @Email(message = "이메일 형식이 올바르지 않습니다.")
+    private String email;
+
+}
---
+++ src/main/java/com/ceos18/springboot/mail/response/EmailConfirmResponseDto.java
@@ -0,0 +1,12 @@
+package com.ceos18.springboot.mail.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class EmailConfirmResponseDto {
+    private String data;
+}
